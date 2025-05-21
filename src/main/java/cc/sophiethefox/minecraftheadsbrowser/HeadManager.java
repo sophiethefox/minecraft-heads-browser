6f8dd350057103f21e170c47a491b20850442425
Sophie <56909681+sophiethefox@users.noreply.github.com>
2025-05-21 17:43:36 +1000

    local heads backup, icon change

--- src/main/java/cc/sophiethefox/minecraftheadsbrowser/HeadManager.java
+++ src/main/java/cc/sophiethefox/minecraftheadsbrowser/HeadManager.java
@@ -10,24 +10,24 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 public class HeadManager {
-    private static final String[] categories = {
-            "alphabet", "animals", "blocks", "decoration", "food-drinks",
-            "humans", "humanoid", "miscellaneous", "monsters", "plants"
-    };
     public Map<String, List<HeadData>> categorizedHeads = new HashMap<>();
 
-    public List<String> activeCategories ;
-    public String searchTerm ;
+    public List<String> activeCategories;
+    public String searchTerm;
     public Integer page;
     public Boolean nbtEdit;
 
@@ -41,10 +41,7 @@ public class HeadManager {
     public ItemStack createSkullFromHeadData(HeadData headData) {
         ItemStack skull = new ItemStack(Items.PLAYER_HEAD);
 
-        GameProfile profile = new GameProfile(
-                headData.uuid != null ? UUID.fromString(headData.uuid) : UUID.randomUUID(),
-                headData.name != null ? headData.name : "Head"
-        );
+        GameProfile profile = new GameProfile(headData.uuid != null ? UUID.fromString(headData.uuid) : UUID.randomUUID(), headData.name != null ? headData.name : "Head");
 
         profile.getProperties().put("textures", new Property("textures", headData.value));
 
@@ -60,20 +57,34 @@ public class HeadManager {
 
     public void fetchAllHeads() {
         Gson gson = new Gson();
-        for (String cat : categories) {
-            try {
-                URL url = URI.create("https://minecraft-heads.com/scripts/api.php?cat=" + cat).toURL();
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-                conn.setRequestMethod("GET");
-                conn.connect();
-                try (InputStreamReader reader = new InputStreamReader(conn.getInputStream())) {
-                    List<HeadData> data = gson.fromJson(reader, new TypeToken<List<HeadData>>() {
-                    }.getType());
-                    categorizedHeads.put(cat, data);
+        try {
+            URL url = URI.create("https://mcheads.sophiethefox.cc/heads.json").toURL();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            try (InputStreamReader reader = new InputStreamReader(conn.getInputStream())) {
+                Type mapType = new TypeToken<Map<String, List<HeadData>>>() {
+                }.getType();
+                categorizedHeads = gson.fromJson(reader, mapType);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Fetching from server failed. Attempting to read local heads.json");
+            try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("assets/minecraft-heads-browser/heads.json")) {
+                if (inputStream == null) {
+                    System.out.println("Could not find file!");
+                    return;
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                Type mapType = new TypeToken<Map<String, List<HeadData>>>() {}.getType();
+
+                categorizedHeads = gson.fromJson(reader, mapType);
+            } catch (Exception e_) {
+                System.out.println("Failed reading local heads.json");
+                e_.printStackTrace();
             }
         }
     }
